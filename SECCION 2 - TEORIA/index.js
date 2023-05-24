4ee1aa25a071099fd265445d96be045cdbbe2390
enriqueaquije <bryanaquije@gmail.com>
2023-05-24 00:18:10 -0500

    primeros pasos Teoria

---
+++ SECCION 2 - TEORIA/index.js
@@ -0,0 +1,16 @@
+var a = 1;
+var b = 2;
+let name = 'Enrique Aquije';
+var vari = true;
+
+console.log(`Hola mundo, este mensaje fue escrito 
+por el futuro programador ${name}`);
+console.log("----------------------------------------");
+console.log(`la suma de los numeros es ${a + b}`);
+console.log("----------------------------------------");
+console.log(vari);
+
+if(vari){
+     console.log("la variable es verdadera")
+}
+
